f380312f6f32686be72dce0124ff090fdc5d3e28
Zak <andresjr195@hotmail.com>
2022-06-22 18:55:25 -0500

    krypto

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 import dummyData from '../utils/dummyData';
+import useFetch from './Hooks/useFetch';
 
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+  // const gifUrl = useFetch({ keyword })
   return (
     <div className='bg-[#181918] m-4 flex flex-1
       2xl:min-w-[450px]
@@ -12,7 +14,7 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
       flex-col p-3 rounded-md hover:shadow-2xl
     '>
       <div className='flex flex-col items-center w-full mt-3'>
-        <div className='flex justify-start w-full mb-6 p-2'>
+        <div className=' w-full mb-6 p-2'>
           <a href='#'>
             <p className='text-white text-base'>From: OcwF8-..6A50</p>
           </a>
@@ -20,7 +22,16 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
             <p className='text-white text-base'>To Pac7F8-..5S870</p>
           </a>
           <p className='text-white text-base'>Amount: 0.01ETH</p>
+          
         </div>
+        <img src={ url }
+               alt='gif'
+               className='w-full h-64 2x:h-96 rounded-md shadow-md'
+          />
+
+          <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl object-cover'>
+            <p className='text-[#37c7da] font-bold'>12/21/2022, 4:33:21 PM</p>
+          </div>
       </div>
     </div>
   )
